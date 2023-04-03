01244c4842ac8172144e4e5a3f2fe5d50fc8dd8d
Kate Fray <katewfray@gmail.com>
2023-04-03 16:00:56 +0000

    added db_test route

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
+
+@app.route('/db_test')
+def db_test:
+    conn = psycopg2.connect("postgres://kfray_renderapp_db_user:xm98rBz6dnCzMowhUlPPe97XI6KPJxlp@dpg-cglfedseoogkndgcqiag-a.oregon-postgres.render.com/kfray_renderapp_db")
+    conn.close()
+    return "Successful connection to kfray_renderapp_db database"
 
 @app.route('/')
 def hello_world():
