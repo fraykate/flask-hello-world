59ef03fdbec7686e1e4ff0ba45830da46fde612f
Kate Fray <katewfray@gmail.com>
2023-04-03 16:13:54 +0000

    fixed db_test -- added db_create

--- app.py
+++ app.py
@@ -3,12 +3,32 @@ import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# test the database connection
 @app.route('/db_test')
-def db_test:
+def db_test():
     conn = psycopg2.connect("postgres://kfray_renderapp_db_user:xm98rBz6dnCzMowhUlPPe97XI6KPJxlp@dpg-cglfedseoogkndgcqiag-a.oregon-postgres.render.com/kfray_renderapp_db")
     conn.close()
     return "Successful connection to kfray_renderapp_db database"
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# create a table in the database
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgres://kfray_renderapp_db_user:xm98rBz6dnCzMowhUlPPe97XI6KPJxlp@dpg-cglfedseoogkndgcqiag-a.oregon-postgres.render.com/kfray_renderapp_db") 
+    # create a cursor
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Successfully created Basketball table"
